3b1072ecc567aa4245f16c6d403d8391924f24c7
aj-ash <Cool-Pan@outlook.com>
2020-06-10 10:38:19 +0800

    feat: 清理 commit

---
+++ UPDATE.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# 更新脚本
+#
+
+cp -rf "$(which bm2str)" ./bin/ && chmod +x ./bin/bm2str
+cp -rf "$(which bmp2grayjpg)" ./bin/ && chmod +x ./bin/bmp2grayjpg
+cp -rf "$(which bmp2jpg)" ./bin/ && chmod +x ./bin/bmp2jpg
+cp -rf "$(which bmp2svg)" ./bin/ && chmod +x ./bin/bmp2svg
+cp -rf "$(which char2uni)" ./bin/ && chmod +x ./bin/char2uni
+cp -rf "$(which jpg2bmp)" ./bin/ && chmod +x ./bin/jpg2bmp
+cp -rf "$(which jpg2grayjpg)" ./bin/ && chmod +x ./bin/jpg2grayjpg
+cp -rf "$(which jpg2png)" ./bin/ && chmod +x ./bin/jpg2png
+cp -rf "$(which jpg2svg)" ./bin/ && chmod +x ./bin/jpg2svg
+cp -rf "$(which link2qr)" ./bin/ && chmod +x ./bin/link2qr
+cp -rf "$(which png2jpg)" ./bin/ && chmod +x ./bin/png2jpg
+cp -rf "$(which png2svg)" ./bin/ && chmod +x ./bin/png2svg
+cp -rf "$(which s2tw)" ./bin/ && chmod +x ./bin/s2tw
+cp -rf "$(which str2bm)" ./bin/ && chmod +x ./bin/str2bm
+cp -rf "$(which str2uni)" ./bin/ && chmod +x ./bin/str2uni
+cp -rf "$(which ts2mp4)" ./bin/ && chmod +x ./bin/ts2mp4
+cp -rf "$(which tw2s)" ./bin/ && chmod +x ./bin/tw2s
+cp -rf "$(which uni2char)" ./bin/ && chmod +x ./bin/uni2char
+cp -rf "$(which uni2str)" ./bin/ && chmod +x ./bin/uni2str
