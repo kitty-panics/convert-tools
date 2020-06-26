43bde81e35033dd3a8530dd287d34d1ee7bcc11b
aj-ash <Cool-Pan@outlook.com>
2020-06-26 23:40:33 +0800

    feat: 新增部分脚本

--- UPDATE.sh
+++ UPDATE.sh
@@ -4,7 +4,7 @@
 # 更新脚本
 #
 
-cp -rf "$(which bm2str)" ./bin/ && chmod +x ./bin/bm2str
+cp -rf "$(which 642str)" ./bin/ && chmod +x ./bin/642str
 cp -rf "$(which bmp2grayjpg)" ./bin/ && chmod +x ./bin/bmp2grayjpg
 cp -rf "$(which bmp2jpg)" ./bin/ && chmod +x ./bin/bmp2jpg
 cp -rf "$(which bmp2svg)" ./bin/ && chmod +x ./bin/bmp2svg
@@ -16,9 +16,11 @@ cp -rf "$(which jpg2svg)" ./bin/ && chmod +x ./bin/jpg2svg
 cp -rf "$(which link2qr)" ./bin/ && chmod +x ./bin/link2qr
 cp -rf "$(which png2jpg)" ./bin/ && chmod +x ./bin/png2jpg
 cp -rf "$(which png2svg)" ./bin/ && chmod +x ./bin/png2svg
+cp -rf "$(which rm2trash)" ./bin/ && chmod +x ./bin/rm2trash
 cp -rf "$(which s2tw)" ./bin/ && chmod +x ./bin/s2tw
-cp -rf "$(which str2bm)" ./bin/ && chmod +x ./bin/str2bm
 cp -rf "$(which str2uni)" ./bin/ && chmod +x ./bin/str2uni
+cp -rf "$(which str264)" ./bin/ && chmod +x ./bin/str264
+cp -rf "$(which svg2png)" ./bin/ && chmod +x ./bin/svg2png
 cp -rf "$(which ts2mp4)" ./bin/ && chmod +x ./bin/ts2mp4
 cp -rf "$(which tw2s)" ./bin/ && chmod +x ./bin/tw2s
 cp -rf "$(which uni2char)" ./bin/ && chmod +x ./bin/uni2char
